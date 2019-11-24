145189de404936cc2b3cbd08932dc3003c188601
Shelby A. Blythe <shelby.blythe@northwestern.edu>
2019-11-24 15:14:21 -0600

    Initial file upload

---
+++ tub-opa_all_open_reads_list.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cdef0671f6aed0971128c3315a3235e0b8791bb67d79f8c2a53ebd162d33ac75
+size 69793832
---
+++ wt_NC13+12_all_open_reads_list.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f90ef5a111e81ad46eec48c9814fd5ca14fe85026e2a5656fd530340662fb44
+size 87308377
